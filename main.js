bb4d8f4cb577b182023edb16d7c46957dd25dab6
Tanzir Rifat <93050315+tanzirrahmanrifat@users.noreply.github.com>
2022-09-20 09:58:10 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let arrName = ["Rahim","Karim","Jamal","Habib","Manik"];
+console.log(arrName);
+arrName.push("Abdul Alim");
+console.log(arrName);
+arrName.pop();
+console.log(arrName);
+arrName.shift();
+console.log(arrName);
+arrName.unshift();
+console.log(arrName);
+arrName.splice(1,2);
+console.log(arrName);
+
+// array splice and splice number show
+
+var newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+console.log(newArr);
+var spliceArr = newArr.splice(3,5);
+console.log(newArr);
+console.log(spliceArr);
+
+// array copy 
+let arr1 = [1,2,3,4,5,6,7,8,9,0];
+numArr1 = arr1;
+console.log(numArr1);
+
